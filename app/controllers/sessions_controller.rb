6e77778e901e280be118eb302fc4dcab07dbeb09
khanhpn-2944 <pham.nhat.khanh@sun-asterisk.com>
2022-03-15 17:20:06 +0700

    Chapter 8 - 9: Basic and Advanced login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: session_params[:email].downcase
+    if user&.authenticate session_params[:password]
+      log_in user
+      session_params[:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_to user
+    else
+      flash[:danger] = t "flash_session.create_fail"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit :email, :password, :remember_me
+  end
+end
